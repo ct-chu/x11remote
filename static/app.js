36a9cafea7e28b6cbc6b01453cca731130a1d2c7
ct-chu <einsteinchu@gmail.com>
2023-10-04 17:37:31 +0800

    ch

--- static/app.js
+++ static/app.js
@@ -92,7 +92,7 @@ function newRect(x,y,w,h,borderclr,fillclr,buttonText,bg) {
   r.x = x;
   r.y = y;
   if ((bg != "") || (bg != null)) {
-    r.graphics.beginStroke(borderclr).beginBitmapFill(img, "no-repeat")
+    r.graphics.beginStroke(borderclr).beginBitmapFill(bg, "no-repeat")
       .drawRect(0,0,w,h);
   } else {
     r.graphics.beginStroke(borderclr).beginFill(fillclr)
